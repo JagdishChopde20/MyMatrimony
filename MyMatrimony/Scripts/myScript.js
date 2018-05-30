cde090c1be846c295560772a7f7e907fb6bed06b
Jagdish Chopde <jagdishchopde@outlook.com>
2018-05-31 01:09:11 +0530

    Added ResponsiveBootstrapToolkit to detect the device width changes in jQuery code. and added the filter panel and set it's visibility on large and mobile devices.

---
+++ MyMatrimony/Scripts/myScript.js
@@ -0,0 +1,67 @@
+﻿
+
+(function ($, document, window, viewport) {
+    var visibilityDivs = {
+        'xs': $('<div class="d-xs-block d-sm-none d-md-none d-lg-none d-xl-none"></div>'),
+        'sm': $('<div class="d-none d-sm-block d-md-none d-lg-none d-xl-none"></div>'),
+        'md': $('<div class="d-none d-md-block d-sm-none d-lg-none d-xl-none"></div>'),
+        'lg': $('<div class="d-none d-lg-block d-sm-none d-md-none d-xl-none"></div>'),
+        'xl': $('<div class="d-none d-xl-block d-sm-none d-md-none d-lg-none"></div>'),
+    };
+    viewport.use('custom', visibilityDivs);
+
+    // Executes once whole document has been loaded
+    $(document).ready(function () {
+        if (viewport.is('xs')) {
+            //console.log('xs');
+            $('#filterDiv').addClass('fixed-bottom').removeClass('show');
+        }
+
+        // Executes in SM, MD and LG breakpoints
+        if (viewport.is('sm')) {
+            //console.log('sm');
+            $('#filterDiv').addClass('fixed-bottom').removeClass('show');
+        }
+
+        // Executes in XS and SM breakpoints
+        if (viewport.is('md')) {
+            //console.log('md');
+            $('#filterDiv').addClass('fixed-bottom').removeClass('show');
+        }
+        //console.log('Current breakpoint:', viewport.current());           
+    });
+
+    $(window).resize(
+        viewport.changed(function () {
+            // Executes only in XS breakpoint
+            if (viewport.is('xs')) {
+                //console.log('xs');
+                $('#filterDiv').addClass('fixed-bottom').removeClass('show');
+            }
+
+            // Executes in SM, MD and LG breakpoints
+            if (viewport.is('sm')) {
+                //console.log('sm');
+                $('#filterDiv').addClass('fixed-bottom').removeClass('show');
+            }
+
+            // Executes in XS and SM breakpoints
+            if (viewport.is('md')) {
+                //console.log('md');
+                $('#filterDiv').addClass('fixed-bottom').removeClass('show');
+            }
+
+            if (viewport.is('lg')) {
+                //console.log('lg');
+                $('#filterDiv').removeClass('fixed-bottom').addClass('show');
+            }
+
+            if (viewport.is('xl')) {
+                //console.log('xl');
+                $('#filterDiv').removeClass('fixed-bottom').addClass('show');
+            }
+            //console.log('Current breakpoint:', viewport.current());
+        })
+    );
+})(jQuery, document, window, ResponsiveBootstrapToolkit);
+
